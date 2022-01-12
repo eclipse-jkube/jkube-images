e05010ac075b80b355425594be7d30e18743daa0
Marc Nuri <marc@marcnuri.com>
2022-01-12 15:40:18 +0100

    feat: jkube-tomcat9 image upgraded to Java 17

--- scripts/test-jkube-tomcat9.sh
+++ scripts/test-jkube-tomcat9.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+set -Eeuo pipefail
+trap 'exit' ERR
+BASEDIR=$(dirname "$BASH_SOURCE")
+source "$BASEDIR/common.sh"
+
+IMAGE="quay.io/jkube/jkube-tomcat9:$TAG_OR_LATEST"
+
+assertContains "$(dockerRun 'id')" "uid=1000 gid=0(root) groups=0(root)" || reportError "Invalid run user, should be 1000"
+
+assertContains "$(dockerRun 'java -version')" 'openjdk version "17.0.1"' || reportError "Invalid Java version"
+
+# S2I scripts
+s2i="$(dockerRun 'ls -la /usr/local/s2i/')"
+assertContains "$s2i" "assemble" || reportError "assemble not found"
+assertContains "$s2i" "run" || reportError "run not found"
+assembleScript="$(dockerRun 'cat /usr/local/s2i/assemble')"
+assertContains "$assembleScript" 'copy_dir bin$' || reportError "Invalid s2i assemble script"
+assertContains "$assembleScript" 'copy_dir deployments$' || reportError "Invalid s2i assemble script"
+assertContains "$assembleScript" 'copy_dir maven$' || reportError "Invalid s2i assemble script"
+
+# Env
+env_variables="$(dockerRun 'env')"
+assertContains "$env_variables" "JAVA_HOME=/opt/java/openjdk$" \
+  || reportError "JAVA_HOME invalid"
+assertContains "$env_variables" "JAVA_VERSION=jdk-17.0.1+12$" \
+  || reportError "JAVA_VERSION invalid"
+assertContains "$env_variables" "CATALINA_HOME=/usr/local/tomcat$" \
+  || reportError "CATALINA_HOME invalid"
+assertContains "$env_variables" "TOMCAT_VERSION=9.0.56$" \
+  || reportError "TOMCAT_VERSION invalid"
+assertContains "$env_variables" "DEPLOYMENTS_DIR=/deployments$" \
+  || reportError "DEPLOYMENTS_DIR invalid"
