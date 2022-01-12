56606bec936c6744841a4b7298eedef29665a638
Marc Nuri <marc@marcnuri.com>
2022-01-12 15:33:06 +0100

    feat: jkube-jetty9 image upgraded to Java 17

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,6 +2,12 @@
 
 TAG_OR_LATEST="${TAG:-latest}"
 
+function dockerRun() {
+  read -ra COMMAND <<<"$1"
+  output=$(docker run  --rm --pull never "$IMAGE" "${COMMAND[@]}" 2>&1)
+  echo "$output";
+}
+
 function assertContains() {
   echo "$1" | grep "$2" > /dev/null
 }
