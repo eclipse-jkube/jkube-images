afc77c49d7cfc63a3e13f07050e0da85518407c9
Marc Nuri <marc@marcnuri.com>
2023-10-24 09:55:16 +0200

    feat: bump jolokia to v1.7.2

--- scripts/test-jkube-java-11.sh
+++ scripts/test-jkube-java-11.sh
@@ -58,7 +58,7 @@ assertContains "$env_variables" "JBOSS_CONTAINER_MAVEN_DEFAULT_MODULE=/opt/jboss
   || reportError "JBOSS_CONTAINER_MAVEN_DEFAULT_MODULE invalid"
 assertContains "$env_variables" "JBOSS_CONTAINER_S2I_CORE_MODULE=/opt/jboss/container/s2i/core/$" \
   || reportError "JBOSS_CONTAINER_S2I_CORE_MODULE invalid"
-assertContains "$env_variables" "JOLOKIA_VERSION=1.7.1$" \
+assertContains "$env_variables" "JOLOKIA_VERSION=1.7.2$" \
   || reportError "JOLOKIA_VERSION invalid"
 assertContains "$env_variables" "AB_JOLOKIA_PASSWORD_RANDOM=true$" \
   || reportError "AB_JOLOKIA_PASSWORD_RANDOM invalid"
