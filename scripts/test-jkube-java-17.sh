c8641c9ccadddce25380600d6cfdf6f048856b2e
Marc Nuri <marc@marcnuri.com>
2024-01-03 13:42:08 +0100

    review: support for Jolokia 2.0.0

--- scripts/test-jkube-java-17.sh
+++ scripts/test-jkube-java-17.sh
@@ -72,7 +72,7 @@ assertContains "$env_variables" "JBOSS_CONTAINER_MAVEN_DEFAULT_MODULE=/opt/jboss
   || reportError "JBOSS_CONTAINER_MAVEN_DEFAULT_MODULE invalid"
 assertContains "$env_variables" "JBOSS_CONTAINER_S2I_CORE_MODULE=/opt/jboss/container/s2i/core/$" \
   || reportError "JBOSS_CONTAINER_S2I_CORE_MODULE invalid"
-assertContains "$env_variables" "JOLOKIA_VERSION=1.7.2$" \
+assertContains "$env_variables" "JOLOKIA_VERSION=2.0.0$" \
   || reportError "JOLOKIA_VERSION invalid"
 assertContains "$env_variables" "AB_JOLOKIA_PASSWORD_RANDOM=true$" \
   || reportError "AB_JOLOKIA_PASSWORD_RANDOM invalid"
