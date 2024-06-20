62c6e5dce362b9d2f67cdd5473c68998148a4184
Marc Nuri <marc@marcnuri.com>
2024-06-20 12:06:02 +0200

    test: thorough test for jkube-java-xx (#54)

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,9 +2,16 @@
 
 TAG_OR_LATEST="${TAG:-latest}"
 
+# Run command in docker container for configured image, the first argument is the command to run in the container
 function dockerRun() {
   read -ra COMMAND <<<"$1"
   output=$(docker run  --rm --pull never "$IMAGE" "${COMMAND[@]}" 2>&1)
+  echo "$output";
+}
+
+# Run command in docker container for configured allowing to pass arguments including quotes and spaces
+function dockerRunE() {
+  output=$(docker run  --rm --pull never "$IMAGE" "${@}" 2>&1)
   echo "$output";
 }
 
